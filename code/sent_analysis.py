dff799d0f6448d4d937dff3aef9cc8e14ea37844
edlor <56137280+edlor@users.noreply.github.com>
2021-11-14 02:32:56 +0100

    Q4 – almost finished

--- code/sent_analysis.py
+++ code/sent_analysis.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.tokenize import sent_tokenize
 
@@ -99,9 +100,70 @@ def sent_of_anime(score):
         (string)
     """
         
-    if score >= 0.05:
+    if score >= 0.2:
         return "Positive"
-    elif score <= -0.05:
+    elif score <= -0.2:
         return "Negative"
     else:
         return "Neutral"
+    
+
+def sent_pie_chart(plt, sent, text_fontsize = 15):
+    """
+    This function creates a matplotlib pie chart
+    
+    Arguments
+        plt  : matplotlib.pyplot plot
+        sent : pandas dataframe column with
+               "Positive", "Negative" or "Neutral"
+    
+    Returns
+        updated plt object
+    """
+    
+    # Code to set custom colors inspired by this
+    # https://stackoverflow.com/questions/35206282/assign-specific-colours-to-data-in-matplotlib-pie-chart
+    
+    labels = [s for s in sent.value_counts().index]
+    
+    colors = {"Positive": "greenyellow",
+              "Neutral": "skyblue",
+              "Negative": "crimson"}
+    
+    plt.pie(sent.value_counts(), 
+            labels = labels,
+            autopct = '%.0f%%',
+            pctdistance = 0.7,
+            startangle = 90,
+            textprops = {'fontsize': text_fontsize},
+            colors = [colors[sent] for sent in labels])
+      
+    return plt
+
+
+def sent_multiple_pie_charts(df, plt, axes, title_size = 20):
+    """
+    This function creates a matplotlib plot
+    with multiple pie charts of the sentiment, 
+    one for each of the anime types
+    
+    Arguments
+        df   : pandas dataframe
+        plt  : matplotlib.pyplot plot
+        axes : plt subplots
+        title_size : (int) size of the title of each subplot
+    
+    Returns
+        updated plt object
+    """
+    
+    anime_types = df_sent.groupby('type').size().keys()
+    
+    for ax, anime_type in zip(axes.flatten(), anime_types):
+        
+        ax.title.set_text(anime_type)
+        ax.title.set_size(title_size)
+        
+        sent_pie_chart(ax, df[df['type'] == anime_type]['sentiment'])
+        
+    return plt
